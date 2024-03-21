7908e0363916a8ac4b695610f2d59e160d10a185
James Escobar <escocodeseth@gmail.com>
2024-03-21 00:03:26 -0400

    inital commit

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,28 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
 
+return (`Delicious ${name}`)
+}
+nameMenuItem("Burrito")
+nameMenuItem("Pizza")
+nameMenuItem("Taco")
+
+function createMenuItem(){
+  let menuItem = {
+    name: "French Toast",
+    price: 10.99,
+    type: "breakfast",
+  }
+}
+
+function addIngredients(ingredient,quant) {
+  quant.push(ingredient);
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
